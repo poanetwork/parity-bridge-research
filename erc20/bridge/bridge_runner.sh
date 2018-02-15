aabeb9385ca6116d407ed35ccd44b1cf06ee4e31
Alexander Kolotov <alexandr.kolotov@gmail.com>
2018-02-15 11:13:34 +0300

    reduce log verbosity by default

--- erc20/bridge/bridge_runner.sh
+++ erc20/bridge/bridge_runner.sh
@@ -5,7 +5,7 @@
 #export RUST_LOG=all
 #export RUST_LOG=error
 #export RUST_LOG=warning
-#export RUST_LOG=info
-export RUST_LOG=debug
+export RUST_LOG=info
+#export RUST_LOG=debug
 
 exec ./bridge --config erc20.toml --database erc20_db.toml
