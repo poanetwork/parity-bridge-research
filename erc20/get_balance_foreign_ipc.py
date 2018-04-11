8adab7e19b897df8d18355863f011b18a8ef2e38
Alexander Kolotov <alexandr.kolotov@gmail.com>
2018-04-11 23:34:56 +0300

    functionality to get balances extended

--- erc20/get_balance_foreign_ipc.py
+++ erc20/get_balance_foreign_ipc.py
@@ -18,7 +18,14 @@ web3 = Web3(Web3.IPCProvider(IPC_file))
 signer = web3.toChecksumAddress("0xf3ee321df87781864f46f6464e764c2827fca73b")
 sender = web3.toChecksumAddress("0x37a30534da3d53aa1867adde26e114a3161b2b12")
 #sender = "0x08c7e1b446520914ba7126325d69fe2863f62413"
+accounts_list = [signer, sender]
 
-for i in [signer, sender]:
+if (len(sys.argv) == 2):
+    if (sys.argv[1] == "--all"):
+        accounts_list = web3.eth.accounts
+    else:
+        sys.exit(1)
+
+for i in accounts_list:
     balance=web3.eth.getBalance(i)
     print(i, ":", web3.fromWei(balance, 'ether'))
--- erc20/get_balance_home_ipc.py
+++ erc20/get_balance_home_ipc.py
@@ -17,7 +17,14 @@ web3 = Web3(Web3.IPCProvider(IPC_file))
 
 signer = web3.toChecksumAddress("0x842eb2142c5aa1260954f07aae39ddee1640c3a7")
 sender = web3.toChecksumAddress("0x37a30534da3d53aa1867adde26e114a3161b2b12")
+accounts_list = [signer, sender]
 
-for i in [signer, sender]:
+if (len(sys.argv) == 2):
+    if (sys.argv[1] == "--all"):
+        accounts_list = web3.eth.accounts
+    else:
+        sys.exit(1)
+
+for i in accounts_list:
     balance=web3.eth.getBalance(i)
     print(i, ":", web3.fromWei(balance, 'ether'))
