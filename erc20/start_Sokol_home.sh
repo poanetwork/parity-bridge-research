a59403b0562ab91514396b230f75b38813c8984c
Alexander Kolotov <alexandr.kolotov@gmail.com>
2018-05-15 14:20:45 +0300

    startup script for Sokol simplified

--- erc20/start_Sokol_home.sh
+++ erc20/start_Sokol_home.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-PARITY_BASE="/home/koal/parity"
-RUNNER="parity_Sokol_home.sh"
-TASK="Sokol_home"
+PARITY_BASE="/home/koal/poa"
+TASK="sokol_home"
+PARITY_CONFIG="Sokol_home.toml"
+TASK_WORK_DIR="${PARITY_BASE}/sokol"
 
-cd ${PARITY_BASE}/${TASK}
+#DEBUG_RPC='-l rpc=trace'
+#EXTRA_CONFIG='--jsonrpc-hosts="all" --jsonrpc-cors null'
 
-exec screen -d -m -U -t "${TASK}" -S "${TASK}.screen" -h 5000 -L -s ${PARITY_BASE}/${RUNNER}
+configdir=`dirname $0`
+cd ${configdir}
+
+source start_common.sh
