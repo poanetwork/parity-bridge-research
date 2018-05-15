9b8a88e64084245109082cbbc4889fa4d3f85281
Alexander Kolotov <alexandr.kolotov@gmail.com>
2018-05-15 17:20:48 +0300

    parity startup scripts for Ropsten simplified

--- erc20/start_Ropsten_foreign.sh
+++ erc20/start_Ropsten_foreign.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
 
+PARITY_BASE="/home/koal/poa"
+TASK="ropsten_foreign"
+PARITY_CONFIG="Ropsten_foreign.toml"
+TASK_WORK_DIR="${PARITY_BASE}/test"
+
+#DEBUG_RPC='-l rpc=trace'
+EXTRA_CONFIG='--no-ancient-blocks'
+
+configdir=`dirname $0`
+cd ${configdir}
+
+source start_common.sh
+
 PARITY_BASE="/home/koal/parity"
 RUNNER="parity_Ropsten_foreign.sh"
 TASK="Ropsten_foreign"
