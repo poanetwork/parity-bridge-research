66667b84f50007b02ea4ad7ac6c7adc157d61173
Alexander Kolotov <alexandr.kolotov@gmail.com>
2018-05-15 17:37:19 +0300

    bridge startup script simplified

--- erc20/bridge/start_bridge.sh
+++ erc20/bridge/start_bridge.sh
@@ -1,9 +1,37 @@
 #!/bin/bash
 
-PARITY_BASE="/home/koal/parity/bridge"
-RUNNER="bridge_runner.sh"
+BRIDGE_BASE="/home/koal/git-repos/parity-bridge-research/erc20/bridge"
 TASK="PoA_bridge"
+BRIDGE_BIN="./bridge"
+CONFIG="sokol_ropsten_config_rpc.toml"
+DATABASE="sokol_ropsten_db.toml"
 
-cd ${PARITY_BASE}
+TASK_WORK_DIR="${BRIDGE_BASE}"
 
-exec screen -d -m -U -t "${TASK}" -S "${TASK}.screen" -h 5000 -L -s ${PARITY_BASE}/${RUNNER}
+if [ `ps ax | grep ${BRIDGE_BIN} | grep ${CONFIG} | wc -l` == "1" ]; then
+    echo "Already exists"
+    exit 2
+fi
+
+if [ `screen -ls ${TASK}.screen | grep ${TASK}.screen | wc -l` == "1" ]; then
+    cd ${TASK_WORK_DIR}
+
+    #export RUST_BACKTRACE=1
+
+    #export RUST_LOG=all
+    #export RUST_LOG=error
+    #export RUST_LOG=warning
+    export RUST_LOG=info
+    #export RUST_LOG=debug
+
+    exec ${BRIDGE_BIN} --config ${CONFIG} --database ${DATABASE}
+
+    exit 1
+fi
+
+curdir=`pwd`
+scr=$0 
+
+cd ${TASK_WORK_DIR}
+
+exec screen -d -m -U -t "${TASK}" -S "${TASK}.screen" -h 5000 -L -s ${curdir}/${scr}
